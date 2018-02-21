4ebc07ceacdfaabb7f977b7bece32df443d23bd0
koiic <ibraismail1990@gmail.com>
2018-02-21 11:00:48 +0100

    add template components and links

---
+++ client/src/components/Contact.vue
@@ -0,0 +1,171 @@
+<template>
+  <!-- welcome section -->
+<!--breadcumb start here-->
+<div>
+<section class="xs-inner-welcome-section fundpress-inner-welcome-section fundpress-inner-bg-2 parallax-window" data-parallax="scroll" data-image-src="assets/images/background/get_in_bg.jpg">
+	<div class="xs-solid-overlay xs-bg-black"></div>
+	<div class="container">
+		<div class="fundpress-inner-welcome-content">
+			<h2 class="color-white">Get In Touch</h2>
+			<ul class="xs-breadcumb fundpress-breadcumb">
+				<li><a href="index-2.html" class="color-white"> Home /</a> Contact</li>
+			</ul>
+		</div>
+	</div>
+</section>
+<!--breadcumb end here--><!-- End welcome section -->
+
+<main class="xs-all-content-wrapper">
+	<!-- contact details section -->
+	<section class="xs-content-section-padding fundpress-contact-details-section xs-contact-details-section">
+	<div class="container">
+		<div class="fundpress-heading-title text-center xs-mb-60">
+			<h2 class="color-navy-blue xs-mb-0">We everywhere for support you</h2>
+		</div><!-- .xs-heading-title .fundpress-heading-title END -->
+
+		<div class="row">
+			<div class="col-md-4">
+				<div class="fundpress-contact-details">
+					<div class="xs-box-shadow xs-mb-40 fundpress-widnow-wraper">
+						<div class="fundpress-window-top">
+							<div id="xs-multiple-map-1" class="xs-map"></div>
+						</div>
+						<div class="fundpress-window-back">
+							<img src="assets/images/contact/contact-info-img-1.png" alt="">
+						</div>
+						<div class="fundpress-window-nav">
+							<a href="#" class="fundpress-window-opener">
+								<i class="fa fa-angle-right"></i>
+							</a>
+						</div>
+					</div>
+					<!-- xs-box-shadow xs-mb-40 fundpress-widnow-wraper -->
+					<ul class="xs-list-item-icon-text">
+						<li><i class="fa fa-phone color-green"></i>+88 00 11 22 33</li>
+						<li><i class="fa fa-envelope-o color-green"></i>name@yourdomain.com</li>
+						<li><i class="fa fa-map-marker color-green"></i>Welington City, Beside Pull, Australia</li>
+					</ul>
+				</div>
+				<!-- fundpress-contact-details -->
+			</div>
+			<div class="col-md-4">
+				<div class="fundpress-contact-details">
+					<div class="xs-box-shadow xs-mb-40 fundpress-widnow-wraper">
+						<div class="fundpress-window-top">
+							<div id="xs-multiple-map-2" class="xs-map"></div>
+						</div>
+						<div class="fundpress-window-back">
+							<img src="assets/images/contact/contact-info-img-2.png" alt="">
+						</div>
+						<div class="fundpress-window-nav">
+							<a href="#" class="fundpress-window-opener">
+								<i class="fa fa-angle-right"></i>
+							</a>
+						</div>
+					</div>
+					<!-- xs-box-shadow xs-mb-40 fundpress-widnow-wraper -->
+					<ul class="xs-list-item-icon-text">
+						<li><i class="fa fa-phone color-green"></i>+09 56 43 768</li>
+						<li><i class="fa fa-envelope-o color-green"></i>name@yourdomain.com</li>
+						<li><i class="fa fa-map-marker color-green"></i>10/A, Hesting City, USA</li>
+					</ul>
+				</div>
+				<!-- fundpress-contact-details -->
+			</div>
+			<div class="col-md-4">
+				<div class="fundpress-contact-details">
+					<div class="xs-box-shadow xs-mb-40 fundpress-widnow-wraper">
+						<div class="fundpress-window-top">
+							<div id="xs-multiple-map-3" class="xs-map"></div>
+						</div>
+						<div class="fundpress-window-back">
+							<img src="assets/images/contact/contact-info-img-3.png" alt="">
+						</div>
+						<div class="fundpress-window-nav">
+							<a href="#" class="fundpress-window-opener">
+								<i class="fa fa-angle-right"></i>
+							</a>
+						</div>
+					</div>
+					<!-- xs-box-shadow xs-mb-40 fundpress-widnow-wraper -->
+					<ul class="xs-list-item-icon-text">
+						<li><i class="fa fa-phone color-green"></i>+00 876 54 34</li>
+						<li><i class="fa fa-envelope-o color-green"></i>name@yourdomain.com</li>
+						<li><i class="fa fa-map-marker color-green"></i>Protus Thom, Prekunak, Africa</li>
+					</ul>
+				</div>
+				<!-- fundpress-contact-details -->
+			</div>
+		</div>
+	</div>
+</section>	<!-- End contact details section -->
+
+	<!-- contact us section -->
+	<section class="xs-section-padding xs-gray-bg-2 fundpress-contact-us-section xs-contact-us-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 content-center">
+				<div class="xs-section-heading fundpress-section-heading">
+					<div class="xs-heading-title fundpress-heading-title">
+						<h2 class="color-navy-blue">Get in touch</h2>
+						<span class="xs-separetor dashed-separetor fundpress-separetor"></span>
+						<p>Fundpress site thoughtfully designed for real humans which means the best user experience for your entire community of donors, fundraisers, customers, and staff.</p>
+					</div><!-- .xs-heading-title .fundpress-heading-title .xs-col-9 END -->
+				</div><!-- .xs-section-heading .fundpress-section-heading .xs-spilit-container END -->
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-8 content-center">
+				<div class="xs-contact-form-wraper">
+					<form action="#" method="POST" id="xs-contact-form">
+						<div class="row xs-margin-0 xs-mb-30">
+							<div class="col-md-6 xs-padding-0 xs-pr-15">
+								<div class="xs-input-group">
+									<label for="xs_contact_name">Name</label>
+									<div class="xs-input-group-addon">
+										<i class="fa fa-user color-green xs-input-addon"></i>
+										<input type="text" name="name" id="xs_contact_name" class="xs-input-control" placeholder="Enter your name...">
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6 xs-padding-0 xs-pl-15">
+								<div class="xs-input-group">
+									<label for="xs_contact_email">email</label>
+									<div class="xs-input-group-addon">
+										<i class="fa fa-envelope-o color-green xs-input-addon"></i>
+										<input type="email" name="email" id="xs_contact_email" class="xs-input-control" placeholder="Enter your email...">
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="xs-input-group xs-mb-30">
+							<label for="xs_contact_subject">subject</label>
+							<div class="xs-input-group-addon">
+								<i class="fa fa-sticky-note color-green xs-input-addon"></i>
+								<input type="text" name="subject" id="xs_contact_subject" class="xs-input-control" placeholder="Subject">
+							</div>
+						</div>
+						<div class="xs-massage-group xs-mb-30">
+							<label for="x_contact_massage">Message</label>
+							<div class="xs-massage-group-addon">
+								<i class="fa fa-pencil color-green xs-input-addon"></i>
+								<textarea name="massage" id="x_contact_massage" class="xs-textarea-control" cols="30" rows="10" placeholder="Enter your message..."></textarea>
+							</div>
+						</div>
+						<div class="xs-submit-wraper content-center">
+							<input type="submit" name="submit" value="get a query" id="xs_contact_submit" class="xs-submit-btn">
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>	<!-- End contact us section -->	
+
+	<!-- map section -->
+	<div class="xs-map-section">
+	<div id="xs-maps" class="xs-map"></div>
+</div>	<!-- End map section -->	
+</main>
+</div>
+</template>
---
+++ client/src/components/header.vue
@@ -0,0 +1,111 @@
+<template>
+  
+		<header class="xs-header-height xs-menu-style-transparent xs-menu-style-border fundpress-header-main-version color-navy-blue">
+			<div class="container">
+				<nav class="xs-menus fundpress-menu">
+					<div class="nav-header">
+						<div class="nav-toggle"></div>
+						<a class="nav-brand nav-logo" href="index-2.html">
+							<img src="assets/images/logo.png" alt="">
+						</a>
+					</div><!-- . END -->
+					<div class="nav-menus-wrapper">
+						<div class="xs-logo-wraper">
+							<a class="nav-brand xs-logo fundpress-logo-v1" href="index-2.html">
+								<img src="assets/images/logo.png" alt="">
+							</a>
+						</div>
+						<ul class="nav-menu">
+							<li><a class="nav-link"><router-link to="/">Home</router-link></a></li>
+							<li><a class="nav-link"><router-link to="/about">About</router-link></a></li>
+							<li><a class="nav-link"><router-link to="/blog">Blog</router-link></a></li>
+							<li><a class="nav-link"><router-link to="/contact">Contact</router-link></a></li>
+						</ul>
+						<div class="xs-navs-button">
+							<ul class="xs-icon-with-text fundpress-icon-menu">
+								<li><a href="#" data-toggle="modal" data-target=".bd-example-modal-lg"><i class="fa fa-unlock-alt color-green"></i>Log In</a></li>
+								<li><a class="nav-link"><router-link to="/add">Start Campaign</router-link></a></li>
+							</ul>
+						</div>
+					</div><!-- .nav-menus-wrapper END -->
+				</nav><!-- .xs-menus .fundpress-menu END -->
+			</div>
+            <div class="modal xs-modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+			<div class="modal-dialog modal-lg">
+				<div class="modal-content">
+					<div class="fundpress-tab-nav-v5">
+						<ul class="nav nav-tabs" role="tablist">
+							<li class="nav-item">
+								<a class="nav-link active" href="#login" role="tab" data-toggle="tab"><router-link to="/login">Login</router-link>
+									
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#signup" role="tab" data-toggle="tab"><router-link to="/register">SignUp</router-link>
+									
+								</a>
+							</li>
+						</ul>
+					</div><!-- fundpress-tab-nav-v3-->
+					<!-- Tab panes -->
+					<div class="tab-content">
+						<div role="tabpanel" class="tab-pane fadeInRights show fade in active" id="login">
+							<form action="#" method="POST" id="xs-login-form">
+								<div class="xs-input-group-v2">
+									<i class="icon icon-profile-male"></i>
+									<input type="text" name="name" id="xs_user_login_name" class="xs-input-control" placeholder="Enter your username">
+								</div>
+								<div class="xs-input-group-v2">
+									<i class="icon icon-key2"></i>
+									<input type="password" name="name" id="xs_login_password" class="xs-input-control" placeholder="Enter your password">
+								</div>
+								<div class="xs-submit-wraper xs-mb-20">
+									<input type="submit" name="submit" value="login now" id="xs_contact_get_action" class="btn btn-warning btn-block">
+								</div>
+								<p class="xs-mb-20">or</p>
+								<div class="xs-submit-wraper xs-mb-20">
+									<input type="submit" name="submit" value="Login with facebook account" id="xs_facebook" class="btn btn-info btn-block">
+								</div>
+								<div class="xs-submit-wraper">
+									<input type="submit" name="submit" value="Login with twitter account" id="xs_twitter" class="btn btn-success btn-block">
+								</div>
+							</form>
+						</div><!-- tab-pane -->
+						<div role="tabpanel" class="tab-pane fadeInRights fade" id="signup">
+							<form action="#" method="POST" id="xs-register-form">
+								<div class="xs-input-group-v2">
+									<i class="icon icon-profile-male"></i>
+									<input type="text" name="name" id="xs_register_name" class="xs-input-control" placeholder="Enter your username">
+								</div>
+								<div class="xs-input-group-v2">
+									<i class="icon icon-envelope2"></i>
+									<input type="email" name="email" id="xs_register_email" class="xs-input-control" placeholder="Enter your email">
+								</div>
+								<div class="xs-input-group-v2">
+									<i class="icon icon-key2"></i>
+									<input type="password" name="name" id="xs_register_password" class="xs-input-control" placeholder="Enter your password">
+								</div>
+								<div class="xs-input-group-v2">
+									<i class="icon icon-key2"></i>
+									<input type="password" name="name" id="xs_register_repeat_pass" class="xs-input-control" placeholder="Enter your confirm password">
+								</div>
+								<div class="xs-submit-wraper xs-mb-20">
+									<input type="submit" name="submit" value="login now" id="xs_register_get_action" class="btn btn-warning btn-block">
+								</div>
+								<p class="xs-mb-20">or</p>
+								<div class="xs-submit-wraper xs-mb-20">
+									<input type="submit" name="submit" value="Login with facebook account" id="xs_register_facebook" class="btn btn-info btn-block">
+								</div>
+								<div class="xs-submit-wraper">
+									<input type="submit" name="submit" value="Login with twitter account" id="xs_register_twitter" class="btn btn-success btn-block">
+								</div>
+							</form>
+						</div><!-- tab-pane -->
+					</div><!-- tab-content -->
+				</div>
+			</div>
+		</div>
+		</header>
+		<!-- header version inner menu closed -->
+        
+</template>
